00e6af366e3bec7cccbab9692cf921b4ab0e5ce8
AJ Piter <amanda.piter@gmail.com>
2020-08-27 22:17:37 -0400

    Create BoxPlot.r

---
+++ DataViz/BoxPlot.r
@@ -0,0 +1,7 @@
+#Example from Data Analysis with R 3.2
+
+> #boxplot
+> boxplot(BostonHousing.df$MEDV ~ BostonHousing.df$CHAS, xlab = "CHAS", ylab = "MEDV")
+> 
+> #boxplot with ggplot
+> ggplot(BostonHousing.df) +geom_boxplot(aes(x = as.factor(CHAS), y = MEDV)) + xlab("CHAS")
