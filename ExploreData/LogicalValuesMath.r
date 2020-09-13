4e849c88af1db0fbcefd89c3b6178f162d09b58f
AJ Piter <amanda.piter@gmail.com>
2020-09-13 16:44:43 -0400

    Update LogicalValuesMath.r

--- ExploreData/LogicalValuesMath.r
+++ ExploreData/LogicalValuesMath.r
@@ -1,3 +1,10 @@
+#Logical data must be inputed as TRUE or FALSE
+#Missing data isn't allowed unless specified 
+#Specify by using NA 
+
+typeof(x=NA) 
+[1] logical 
+
 #Logical Values can be used in math 
 #False is 0 in math equations 
 True is 1 in math equations 
