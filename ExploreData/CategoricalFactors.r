5ce60138be99211ea5d9ea02ce453decad2579ae
AJ Piter <amanda.piter@gmail.com>
2020-09-13 16:32:07 -0400

    Create CategoricalFactors.r

---
+++ ExploreData/CategoricalFactors.r
@@ -0,0 +1,20 @@
+#Categorical values are 
+
+#factors convert categorical data to a numbered indiex 
+#numbers are typically assigned alphabetically 
+
+str(object = factor(x = c("Small", "Medium", "Large") 
+[1] Factor with 3 levels "Large" "Medium" "Small" 3 2 1 
+
+#to specify the order of the factos use the levels argument 
+sizes <- factor(x=c("medium", "small", "large") levels = c("small", "medium", "large")
+    ordered = TRUE) 
+str(object = sizes) 
+table (sizes) 
+
+#ProTip: Check your factor levels to make sure you don't have 
+# Male, male, and M 
+levels(variable) 
+levels(gender) 
+[1] "Male" "Female" 
+
