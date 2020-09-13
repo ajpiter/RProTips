eece4085d24b5cd82054fcece501b418819786ab
AJ Piter <amanda.piter@gmail.com>
2020-09-13 16:46:29 -0400

    Update NA.r

--- ExploreData/NA.r
+++ ExploreData/NA.r
@@ -1,3 +1,12 @@
+#NA data could exist but dosent. Has structure. 
+#Null lack of structure where data could exisit. 
+#NAN not a number ie. 0/0 
+
+NA #default NA value is logical 
+NA_real 
+NA_integer 
+NA_Character
+
 #if you compare missing NA values, the result will always be NA 
 4 > NA 
 [1] NA 
