cae0789f8edb0e5fa5c2f1cfb9e44363d5d1c24d
AJ Piter <amanda.piter@gmail.com>
2020-09-13 16:01:22 -0400

    Create CleaningAddresses.r

---
+++ DataCleaning/CleaningAddresses.r
@@ -0,0 +1,11 @@
+
+
+streetNumber <- 1234
+unitNumber <- "Apt 12"
+streetName <- "Main Street" 
+
+streetAddress <- paste(streetNumber, streetName, unitNumber) 
+[1] 1234 Main Street Apt 12 
+
+#since unitNumber and streetName are characters 
+#streetAddress will also be a character 
