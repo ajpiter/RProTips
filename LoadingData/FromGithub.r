613c59dd12651bfd9acede687fe275c305155a0b
AJ Piter <amanda.piter@gmail.com>
2020-09-20 16:37:07 -0400

    Create FromGithub.r

---
+++ LoadingData/FromGithub.r
@@ -0,0 +1,9 @@
+#Files from github must be in a csv format 
+
+#I downloaded my file from “ajpiter R ProTips West Roxbury”
+#The link to the GitHub repository where I downloaded the file is [GitHub Repository] https://github.com/ ajpiter/RProTips/blob/master/Projects/WestRoxburyHomes/WestRoxbury.csv
+
+#To save the dataset as an object, I used the following code:
+WestRoxbury.df <- read.csv("https://github.com/ ajpiter/RProTips/blob/master/Projects/WestRoxburyHomes/WestRoxbury.csv") 
+
+The original file was from Data Analytics with R 
