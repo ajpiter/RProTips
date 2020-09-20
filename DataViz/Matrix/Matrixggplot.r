a801b9b6a61f38bf2eb685ae3249b25fd8df1bec
AJ Piter <amanda.piter@gmail.com>
2020-09-20 15:46:18 -0400

    Update Matrixggplot.r

--- DataViz/Matrix/Matrixggplot.r
+++ DataViz/Matrix/Matrixggplot.r
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(reshape2)
 
+#Named: ggplot Matrix Base in PDF Output 
 #To determine if any of the Remodel Factors have an impact on our dataset we 
 #Create a Matrix using ggplot 
 cor.mat <- round(cor(WestRoxbury.df), 2)
@@ -10,6 +11,7 @@ ggplot(melted.cor.mat, aes(x=Var1, y=Var2, fill = value)) +
   geom_text(aes(x=Var1, y=Var2, label = value))
 
 ----- 
+#Named: ggplot Matrix Label Update in PDF Output 
 #Improving Correlation Matrix 
 #The above Matrix is really hard to read 
 #To improve legiability we can change the orientation of the labels on the X axis and update the colors. 
@@ -23,7 +25,7 @@ ggplot(data = melted.cor.mat, aes(x=Var1, y=Var2, fill = value)) +
   theme(axis.text.x = element_text(angle = 45, size = 9, hjust = 1))
   
   ----- 
-  
+#Named: ggplot Matrix Select Variables in PDF Output 
 #To Make it More legible 
 #Narrow Variables Using select() to help narrow down our plot 
 
