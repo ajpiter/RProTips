4c17415fb94be5db2ec88c4e09a9e303ac732b8f
AJ Piter <amanda.piter@gmail.com>
2020-09-21 11:39:58 -0400

    Create piping%>%.r

---
+++ DataCleaning/tidydeplyr/piping%>%.r
@@ -0,0 +1,9 @@
+%<% symbol represents piping 
+
+Piping is only used in the following libraries 
+tidyverse 
+mdgrittr 
+dplyr 
+
+NewDataFrameName <- OldDataFrameName %>% 
+  filter(ColumnName == "Conte
