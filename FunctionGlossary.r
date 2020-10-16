f719e9170ed329ba97186b0fc16b3dd0bfd046f4
AJ Piter <amanda.piter@gmail.com>
2020-10-16 01:28:14 -0400

    Update FunctionGlossary.r

--- FunctionGlossary.r
+++ FunctionGlossary.r
@@ -20,6 +20,9 @@ sample() #sample rows
 set.seed() #used to get the same data partitionaing when re-running the split of training/testing/validation 
 set.diff() #used to ensure no overlap in new tables when partitioning dataframe 
 
+#Ready for Visualization 
+.bincode() #Bin a numeric vector and return integer codes for the binning, RM.bin=4 if 4<RM<=5; RM.bin=5 if 5<RM<=6;
+aggregate() list() #to tabulate counts 
 
 is.na() #True or FALSE if a value is = to "na" 
 
