915b225609004098aedc9ff1a99c9aaa4535ae0b
AJ Piter <amanda.piter@gmail.com>
2020-10-16 00:10:07 -0400

    Create DimHeadViewNames.r

---
+++ LoadingData/DimHeadViewNames.r
@@ -0,0 +1,8 @@
+Explore data
+
+dim(housing.df)       #find the dimension of data 
+dim(housing.df)[1]    #number of rows
+dim(housing.df)[2]    #number of columns
+head(housing.df)      #show the first six rows
+View(housing.df)      #show all the data in a new tab
+names(housing.df)     #show names of the columns (or variables) 
