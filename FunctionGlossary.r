14c0a7572bd3c23748ebb65123038deba8bb8f69
AJ Piter <amanda.piter@gmail.com>
2020-10-16 01:09:56 -0400

    Create FunctionGlossary.r

---
+++ FunctionGlossary.r
@@ -0,0 +1,24 @@
+
+#Exploring Dataframe/Variables 
+class() #shows the class of a variable 
+dim() #Dimensions of dataframe 
+head() #Shows first 6 rows of dataframe 
+lenght() #length of a dataframe (how many rows) 
+levels() shows the levels of a factor variable 
+names() #Shows the names of variables 
+str() #shows the structure of the dataframe 
+summary() #shows the stats of a dataframe (min, max, mean etc) 
+View() #opens dataframe on a new tab 
+
+#Names 
+names() #Shows the names of variables 
+colnames() #selects the names of variables 
+row.names() #selects the names of rows 
+
+#Sampling 
+sample() #sample rows
+set.seed() #used to get the same data partitionaing when re-running the split of training/testing/validation 
+set.diff() #used to ensure no overlap in new tables when partitioning dataframe 
+
+
+is.na() #True or FALSE if a value is = to "na" 
