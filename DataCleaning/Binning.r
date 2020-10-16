282cb607ea49fbcac84563de95e6ba633c8efc4f
AJ Piter <amanda.piter@gmail.com>
2020-10-16 01:31:02 -0400

    Update and rename Binning.r to BinningAggregate.r

--- DataCleaning/Binning.r
+++
@@ -1,5 +0,0 @@
-#Create bins of size 1 for RM
-housing.df$RM.bin<-.bincode(housing.df$RM, c(1:9))
-
-# .bincode(): Bin a numeric vector and return integer codes for the binning
-# e.g., RM.bin=4 if 4<RM<=5; RM.bin=5 if 5<RM<=6;
