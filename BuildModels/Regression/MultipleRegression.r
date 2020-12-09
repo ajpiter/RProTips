e1fb40d4053a0af97dddf7def7a98077bb16294a
AJ Piter <amanda.piter@gmail.com>
2020-12-08 23:20:20 -0500

    Update MultipleRegression.r

--- BuildModels/Regression/MultipleRegression.r
+++ BuildModels/Regression/MultipleRegression.r
@@ -1,1 +1,17 @@
-#
+#Create the best model by minimizing the sum of the squared residuals 
+#Predict Y using X and X2 
+
+#Create Multiple Regression Model 
+object <- lm(data$variabley~data$variablex + data$variablex2) 
+
+#view the results 
+summary(object) 
+#if the F-test P-value is almost 0 then one of the variables is helpful 
+#r^2 and adjusted r^2, the higher the value (closer to 1) the better the model
+
+#plot the results 
+plot(object) 
+
+#create indivdual plots of y vs xs 
+plot(data$variabley~data$variablex)
+plot(data$variabley~data$variablex2)
