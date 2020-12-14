0e1806fbda20007bf0e3c6926c20365c91157d6e
AJ Piter <amanda.piter@gmail.com>
2020-12-13 21:20:08 -0500

    Update PackageGlossary.r

--- PackageGlossary.r
+++ PackageGlossary.r
@@ -4,6 +4,12 @@
 library(readr) #quickly import csv files in R 
 library(kntr) #helps with r markdown files 
 
-library(dplyr) #create tidy data using dplyr verbs
 
+
+library(rvest) #used for web scraping tables
+library(ggplot2) #used for visualization
+library(dplyr) #used for data manipulation
+library(lattice) #used for complex data visualization
+library(car) #used for making a scatterplot matrix
+library(rstudioapi) #library for getting RStudio version
 library(dummies) #create dummy variables 
