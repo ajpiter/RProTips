ce91753e608942df6229734df8094d9c04046f91
AJ Piter <amanda.piter@gmail.com>
2020-12-13 21:38:18 -0500

    Update CreateVariable.r

--- DataCleaning/CreateVariable.r
+++ DataCleaning/CreateVariable.r
@@ -12,4 +12,14 @@ HouseProperties$PredictPrice2 <- with(HouseProperties, -3729000 +
                         (1759 *HouseProperties$yr_built))
                         
 #Create a new variable by concatenating strings 
-FullName <- 
+FullName <- paste0(data$firstname, data$lastname) 
+#use function paste0() to join strings without spaces 
+paste0("butter", "fly") 
+[1] "butterfly"
+
+#Create a new variable by concatenating a string and numeric
+Course <- paste0(data$CourseCode, data$CourseNumber) 
+#paste() and paste0() can join multiple data types 
+y2k <- paste0("NYE", "2000") 
+y2k #prints the variable 
+[1] "NYE2000"
