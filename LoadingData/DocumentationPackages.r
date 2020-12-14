011c58f9b35fc54b3b2bf7ce1f8403444dc497dc
AJ Piter <amanda.piter@gmail.com>
2020-12-13 21:06:43 -0500

    Create DocumentationPackages.r

---
+++ LoadingData/DocumentationPackages.r
@@ -0,0 +1,12 @@
+#Documentation is important if you are 
+
+#Find the version of R you are using 
+>version 
+
+#Document packages first find the version number for the package
+sessioninfo(packagename)
+sessioninfo(tidyr)
+
+#list the package version number next to the library() when you load package 
+library(ggplot2) #version 3.3.2
+library(plyr) #verson 1.8.6 
