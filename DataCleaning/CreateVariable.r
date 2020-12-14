8e1451b332c7c953a94b50046a72bb991ac84def
AJ Piter <amanda.piter@gmail.com>
2020-12-13 22:10:06 -0500

    Update CreateVariable.r

--- DataCleaning/CreateVariable.r
+++ DataCleaning/CreateVariable.r
@@ -4,6 +4,18 @@ datatable$newvariablename <- how new variable should be calculated
 #Calculate Rate 
 data$Rate <- data$occurrs / data$allobservations 
 
+#Create a Categorical Variable 
+data$NewCategory <- ifelse(data$Variable <= 0.01, "Low", ifelse(data$variable > 0.01 & data$variable <= 0.025,
+                                                     "Med", "High"))
+
+#Another method to create Categorical Variable
+data$NewVariable <- NA
+NewVariable <- data$OldVariable
+NewVariable <- ifelse(NewVariable <= 0.01, "Low", ifelse(NewVariable > 0.01 & NewVariable <= 0.025,
+                                                     "Med", "High"))
+NewVariable
+data$NewVariable <- cbind(NewVariable)
+
 #Create a new variable based off a regression model 
 HouseProperties$PredictPrice2 <- with(HouseProperties, -3729000 + 
                         (65166 *HouseProperties$sqft_living) + 
