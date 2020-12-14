1c46bc2f35a93d2ad4c60384f0ac836b56194214
AJ Piter <amanda.piter@gmail.com>
2020-12-13 22:02:17 -0500

    Update ScatterPlotGgplot.r

--- DataViz/ScatterPlot/ScatterPlotGgplot.r
+++ DataViz/ScatterPlot/ScatterPlotGgplot.r
@@ -1,3 +1,6 @@
+#GGPlot Cheat Sheet
+# https://rstudio.com/wp-content/uploads/2015/03/ggplot2-cheatsheet.pdf
+
 #Name: Color to Highligh Variable
 #Change the color of dots based on 3rd variable to describe relationship 
 ggplot(data = data) + geom_point(mapping = aes(x=variablex, y=variabley, color = variablex2) 
@@ -16,3 +19,8 @@ ggplot(data = data) + geom_point(mapping = aes(x=variablex, y=variabley, shape =
 -----
 #Name: Transparency for Continuous Data 
 ggplot(data = data) + geom_point(mapping = aes(x=variablex, y=variabley, alpha = variablex2) 
+
+-----
+#Name: Add a Regression Line to the Scatterplot
+ggplot(data, aes(variable, variable2)) + stat_summary(fun.data= mean_cl_normal) + geom_smooth(method='lm')                             
+                                 ]
