48367a022134ec4b687f9a2646c8b3be38872194
AJ Piter <amanda.piter@gmail.com>
2020-12-15 18:49:34 -0500

    Create Character2Logical.r

---
+++ DataCleaning/Convert/Character2Logical.r
@@ -0,0 +1,4 @@
+#A logicial class type is a binary True False value 
+
+#Convert a variable from character to logical values using as.logical()
+DR121520$SUPERVISOR_IND <- as.logical(DR121520$SUPERVISOR_IND)
