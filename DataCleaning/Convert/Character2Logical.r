1a3e1d377899e1ab171b798db9ea80e3bd7b5b35
AJ Piter <amanda.piter@gmail.com>
2020-12-15 19:51:05 -0500

    Update Character2Logical.r

--- DataCleaning/Convert/Character2Logical.r
+++ DataCleaning/Convert/Character2Logical.r
@@ -2,3 +2,8 @@
 
 #Convert a variable from character to logical values using as.logical()
 DR121520$SUPERVISOR_IND <- as.logical(DR121520$SUPERVISOR_IND)
+
+#If the values for the variable are not already "TRUE", "FALSE", "T", or "F" you will first need to convert the values 
+#Here there was a binary "Y", "N" variable which was converted to "TRUE", "FALSE"
+DataTable$Variable <- ifelse(DataTable$Variable == "Y", "TRUE", "FALSE") 
+DataTable$Variable <- as.logical(DataTable$Variable)
