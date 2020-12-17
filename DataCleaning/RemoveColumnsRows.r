f5f0071d3ffa2562d48d27a02961f906644f1575
AJ Piter <amanda.piter@gmail.com>
2020-12-16 19:00:42 -0500

    Update RemoveColumnsRows.r

--- DataCleaning/RemoveColumnsRows.r
+++ DataCleaning/RemoveColumnsRows.r
@@ -1,4 +1,28 @@
+#Removing Columns in Base R 
+
 #Remove Unessary Rows, and Columns [Row, Column]
 HomeInventoryMetro.df<- HomeInventoryMetro.df[-1,] #row
 HomeInventoryMetro.df<- HomeInventoryMetro.df[,-1:-2]#columns
 HomeInventoryMetro.df<- HomeInventoryMetro.df[,-2] #columns
+
+----- 
+#Removing Columns using TidyVerse 
+library(tidyverse)
+
+#Removing Columns by Name, removes the two specified columns 
+my_data %>% select(-Column1, -Column50)
+
+#Removing all columns between two column names
+my_data %>% select(-(Column1:Column50))
+
+#Removing all columns whose name starts with “12.15.20”:
+my_data %>% select(-starts_with("12.15.20"))
+
+# Drop column 1
+my_data %>% select(-1)
+
+# Drop columns 1 to 3
+my_data %>% select(-(1:3))
+
+# Drop columns 1 and 3 but not 2
+my_data %>% select(-1, -3)
