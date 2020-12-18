104cf99de744484f6f15084753e763e97d92db32
AJ Piter <amanda.piter@gmail.com>
2020-12-18 13:10:35 -0500

    Update StringConcatenation.r

--- DataCleaning/StringConcatenation.r
+++ DataCleaning/StringConcatenation.r
@@ -1,12 +1,7 @@
-#deafault paste()
-paste("butter", "fly") 
-[1] "butter fly" 
+#paste() and paste0() are the two most common functions for editing string names
 
-#use argumenet sep = "" to remove space 
-paste("butter", "", "fly") 
-[1] "butterfly" 
-
-#use function paste0() to join strings without spaces 
+-----
+#Name: Paste0() joins strings without spaces 
 paste0("butter", "fly") 
 [1] "butterfly"
 
@@ -14,3 +9,12 @@ paste0("butter", "fly")
 y2k <- paste0("NYE", "2000") 
 y2k #prints the variable 
 [1] "NYE2000"
+
+-----
+#paste() adds a space between each 
+paste("butter", "fly") 
+[1] "butter fly" 
+
+#use argumenet sep = "" to remove space 
+paste("butter", "", "fly") 
+[1] "butterfly" 
