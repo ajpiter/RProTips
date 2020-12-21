35f31e9369417ff4fa46427cae6040cf9406f074
AJ Piter <amanda.piter@gmail.com>
2020-12-21 16:38:06 -0500

    Update PackageGlossary.r

--- PackageGlossary.r
+++ PackageGlossary.r
@@ -1,15 +1,18 @@
 #Below is a list of packages, and thier use case in R 
 
-#Import/Export
+##### Import/Export #####
 library(readr) #quickly import csv files in R 
 library(kntr) #helps with r markdown files 
-
-
-
+library(openxlsx) #Export to Excel without Java Dependencies 
+library(xlsx) #Export to Excel needs Java 
 library(rvest) #used for web scraping tables
+
+##### Data Visualization #####
 library(ggplot2) #used for visualization
-library(dplyr) #used for data manipulation
 library(lattice) #used for complex data visualization
 library(car) #used for making a scatterplot matrix
+
+##### Other #####
 library(rstudioapi) #library for getting RStudio version
 library(dummies) #create dummy variables 
+library(dplyr) #used for data manipulation
