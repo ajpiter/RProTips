63c619b2b4066087b7d2955e95c94e839a9065a3
AJ Piter <amanda.piter@gmail.com>
2020-12-22 18:32:10 -0500

    Update ExportXLSX.r

--- LoadingData/ExportXLSX.r
+++ LoadingData/ExportXLSX.r
@@ -7,3 +7,14 @@ openxlsx::write.xlsx(Data, file = "ExcelFileName.xlsx")
 library(xlsx) #saves Excel files, needs Java 
 setwd("/Users/UserName/Documents/Folder/Subfolder")
 write.xlsx(Data, file = "ExcelFileName.xlsx")
+
+##### Export XLSX with today's date appended file name #####
+setwd("/Users/UserName/Documents/Folder/Subfolder")
+library(openxlsx)
+SysDate <- Sys.Date()
+OutputFile <- paste0(SysDate,"DiversityRatios.xlsx")
+openxlsx::write.xlsx(DiversityRatios, file = OutputFile, keepNA = TRUE, 
+                     overwrite = FALSE)
+
+#library(xlsx)
+#write.xlsx(DiversityRatios, file = OutputFile) #uses xlsx package, has Java dependendies 
