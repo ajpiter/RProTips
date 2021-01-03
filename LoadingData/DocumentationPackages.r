29fcd92319c36879fa1f843d277d6d164aebaa5f
AJ Piter <amanda.piter@gmail.com>
2021-01-03 16:30:41 -0500

    Update DocumentationPackages.r

--- LoadingData/DocumentationPackages.r
+++ LoadingData/DocumentationPackages.r
@@ -1,11 +1,11 @@
-#Documentation is important if you are 
+#Documentation is important if you are multiple users are working on a project or working from multiple environments
 
 #Find the version of R you are using 
->version 
+version 
 
 #Document packages first find the version number for the package
-sessioninfo(packagename)
-sessioninfo(tidyr)
+library(sessioninfo)
+session_info(pkgs = NULL, include_base = FALSE)
 
 #list the package version number next to the library() when you load package 
 library(ggplot2) #version 3.3.2
