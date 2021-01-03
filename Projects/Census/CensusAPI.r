8ea2fde003c1791bae4877832aaa1ff0bb1de0ee
AJ Piter <amanda.piter@gmail.com>
2021-01-03 17:59:21 -0500

    Create CensusAPI.r

---
+++ Projects/Census/CensusAPI.r
@@ -0,0 +1,5 @@
+
+#Request Census API: https://api.census.gov/data/key_signup.html
+#Census API User Guide: https://www.census.gov/data/developers/guidance/api-user-guide.html
+#Datasets Available: https://api.census.gov/data.html
+
