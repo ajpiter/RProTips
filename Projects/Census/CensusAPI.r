2da90e42e11b57c112def88a540a4fdfe7870702
AJ Piter <amanda.piter@gmail.com>
2021-01-07 17:35:03 -0500

    Update CensusAPI.r

--- Projects/Census/CensusAPI.r
+++ Projects/Census/CensusAPI.r
@@ -2,4 +2,5 @@
 #Request Census API: https://api.census.gov/data/key_signup.html
 #Census API User Guide: https://www.census.gov/data/developers/guidance/api-user-guide.html
 #Datasets Available: https://api.census.gov/data.html
+#Note: It took 72 hours for my census API key to be emailed to me after I submitted a request 
 
