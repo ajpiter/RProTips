5d2658c5bdf4b0d4058450dbcc88a42ae1f31d5d
AJ Piter <amanda.piter@gmail.com>
2021-01-07 18:07:22 -0500

    Update ReplaceColumnName.r

--- DataCleaning/ReplaceColumnName.r
+++ DataCleaning/ReplaceColumnName.r
@@ -2,8 +2,17 @@
 #You could also create shorter names to decrease the charters in your code 
 #I tend to use longer more descriptive column names which helps when returning to code later
 
+----- #Change Based on Old Column Names ----- 
+names(DataFrame)[names(DataFrame) == "OldColumnName"] <- "NewColumnName"
+names(OrlGameLog)[names(OrlGameLog) == "Rk"] <- "GameRank"
+names(OrlGameLog)[names(OrlGameLog) == "G"] <- "SeasonGame"
+
+---- #Change Based on Column Index -----
 colnames(housing.df)[1] <-c("TOTAL_VALUE")      #from TOTAL.VALUE to TOTAL_VALUE
 names(housing.df)[1]     #check the name of the first variable (before and after) 
+
+colnames(OrlGameLog)[4] <-c("Location") 
+colnames(housing.df)[5] <-c("Opponent") 
 
 #Another method to change the name of a column
 names(data)[1] <- "New Column Name" #change the name of the column
