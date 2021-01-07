42e16adfc3aabd62fbb1a84bbcbadb3c8dd0b3ab
AJ Piter <amanda.piter@gmail.com>
2021-01-07 17:11:03 -0500

    Delete DummyVariables.r

--- DataCleaning/Convert/DummyVariables.r
+++
@@ -1,8 +0,0 @@
-#Categorical to Factor 
-#Don't forget to drop one of the dummy results
-
-install.packages("dummies") # need to install packages, named dummies
-library(dummies)# call the package 
-
-housing.df<-dummy.data.frame(housing.df, sep= ".")# create dummies in housing.df
-housing.df<-housing.df[, 1:15]]# remove16th column, keeping only 1st -15th
