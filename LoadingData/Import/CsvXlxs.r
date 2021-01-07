ce2ee98e274414417fe25c737662f8214ded5138
AJ Piter <amanda.piter@gmail.com>
2021-01-07 16:23:50 -0500

    Update CsvXlxs.r

--- LoadingData/Import/CsvXlxs.r
+++ LoadingData/Import/CsvXlxs.r
@@ -71,3 +71,22 @@ mean(variable.df$TOTAL_VALUE)     #Mean of of the first column
 
 #Summary Stats on Dataframe 
 summary(variable.df) 
+
+----- #DataCamp Examples -----
+
+#CSV Example 
+# Load the readr package
+library(readr)
+# Import potatoes.csv with read_csv(): potatoes
+potatoes <- read_csv("potatoes.csv")
+
+
+#TXT Example, specifing Column Names 
+library(readr) 
+# Column names
+properties <- c("area", "temp", "size", "storage", "method",
+                "texture", "flavor", "moistness")
+# Import potatoes.txt: potatoes
+potatoes <- read_tsv("potatoes.txt", col_names = properties)
+# Call head() on potatoes
+head(potatoes)
