ea65b19ad196d4264c14557724df21a88b92bb23
AJ Piter <amanda.piter@gmail.com>
2021-01-07 18:02:41 -0500

    Update CensusAPI.r

--- Projects/Census/CensusAPI.r
+++ Projects/Census/CensusAPI.r
@@ -2,5 +2,9 @@
 #Request Census API: https://api.census.gov/data/key_signup.html
 #Census API User Guide: https://www.census.gov/data/developers/guidance/api-user-guide.html
 #Datasets Available: https://api.census.gov/data.html
-#Note: It took 72 hours for my census API key to be emailed to me after I submitted a request 
+#Note: It took 72 hours for my census API key to be emailed to me after I submitted a request
+
+---- #Documentation -----
+#Geography's in tidycensus
+#https://walker-data.com/tidycensus/articles/basic-usage.html#geography-in-tidycensus
 
