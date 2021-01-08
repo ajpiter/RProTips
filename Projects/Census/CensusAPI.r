0e1fbecbcf45e2ad9ccd8e16430f7f31dcaa4888
AJ Piter <amanda.piter@gmail.com>
2021-01-07 20:54:47 -0500

    Update CensusAPI.r

--- Projects/Census/CensusAPI.r
+++ Projects/Census/CensusAPI.r
@@ -5,6 +5,14 @@
 #Note: It took 72 hours for my census API key to be emailed to me after I submitted a request
 
 ---- #Documentation -----
-#Geography's in tidycensus
+#Geography's Available in tidycensus
 #https://walker-data.com/tidycensus/articles/basic-usage.html#geography-in-tidycensus
+
+----- #Table Codes -----
+#Table Codes: https://censusreporter.org/topics/table-codes/
+#B##0001
+#Where ## indicates the subject area and 1 indicates the table number within that subject area 
+#B01001 is the Sex by Age table 
+#B01002 is the Median Age by Sex table 
+#B01003 is Total Population table 
 
