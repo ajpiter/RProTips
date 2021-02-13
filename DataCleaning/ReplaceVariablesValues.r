ef181fb490b9289b60224277326a691faf6864a5
AJ Piter <amanda.piter@gmail.com>
2021-02-12 22:45:13 -0500

    Update ReplaceVariablesValues.r

--- DataCleaning/ReplaceVariablesValues.r
+++ DataCleaning/ReplaceVariablesValues.r
@@ -4,5 +4,12 @@
 DataTable$Variable = as.character(sub(" .*", "", DataTable$Variable)) #removes everything after the space
 DataTable$Variable = as.character(sub(",.*", "", DataTable$Variable)) #removes everything after the comma 
 
-To add USA at the end of each instance for the variable Address
+#To add USA at the end of each instance for the variable Address
 DataTable$Address = as.character(sub(" .*", "USA", DataTable$Address)) #adds USA at the end of the address data 
+
+
+#Use BRK for all BRK and NJN games 
+Vucevic$Opponent[Vucevic$Opponent == "NJN"] <- "BRK"
+
+#Use CHO for all CHO and CHA games 
+Vucevic$Opponent[Vucevic$Opponent == "CHA"] <- "CHO"
